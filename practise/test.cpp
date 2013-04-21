b58c696bfafc0938699a2e8262ae1a0bc163c111
lisyarus <lisyarus@gmail.com>
2013-04-21 18:59:56 +0400

    arctg test added

---
+++ practise/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <utility/profiler.tcc>
+#include <cmath>
+
+extern "C"
+{
+ 
+    float my_arctg (float x);
+ 
+}
+
+int main ( )
+{
+    while(true) {
+        float x;
+        std::cin >> x;
+        std::cout << my_arctg(x) << ' ' << tan(my_arctg(x)) << std::endl;
+    }
+}
